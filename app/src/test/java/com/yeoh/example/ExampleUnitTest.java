492e7ee9f2ac8df9fa26d817b565c2d104557dd6
JingYeoh <yangjing9611@foxmail.com>
2018-11-08 15:34:41 +0800

    :construction: update gradle

--- app/src/test/java/com/yeoh/example/ExampleUnitTest.java
+++ app/src/test/java/com/yeoh/example/ExampleUnitTest.java
@@ -1,6 +1,5 @@
 package com.yeoh.example;
 
-import com.yeoh.mock.sub.Caller;
 import org.junit.Test;
 
 /**
@@ -12,7 +11,7 @@ public class ExampleUnitTest {
 
     @Test
     public void addition_isCorrect() throws ClassNotFoundException {
-        Caller.mock();
+//        Caller.mock();
         assert int[].class == Class.forName("[I");
         assert String[].class == Class.forName("[Ljava.lang.String;");
     }
